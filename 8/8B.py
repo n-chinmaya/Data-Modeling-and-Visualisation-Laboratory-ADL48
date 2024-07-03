8fa4572bfa2efe1fb92d500fb64f92ac8f8b69de
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2024-07-03 20:05:37 +0530

    made 8B easier and short

--- 8/8B.py
+++ 8/8B.py
@@ -1,48 +1,28 @@
-def write_to_file(filename, lines):
-  with open(filename, 'w') as file:
-    file.writelines(lines)
-
-def count_characters(text):
-  uppercase_count = 0
-  lowercase_count = 0
-  digit_count = 0
-
-  for char in text:
-    if char.isupper():
-      uppercase_count += 1
-    elif char.islower():
-      lowercase_count += 1
-    elif char.isdigit():
-      digit_count += 1
-
-  return uppercase_count, lowercase_count, digit_count
+def count_characters(file_name):
+    with open(file_name, 'r') as file:
+        text = file.read()
+        upper_case_count = sum(1 for char in text if char.isupper())
+        lower_case_count = sum(1 for char in text if char.islower())
+        digit_count = sum(1 for char in text if char.isdigit())
+        return upper_case_count, lower_case_count, digit_count
 
 def main():
-  filename = "user_text.txt"
-  lines = []
+    file_name = "user_input.txt"
+    with open(file_name, 'w') as file:
+        x=int(input("Enter the numner of lines (5-6): "))
+        print("Enter 5-6 lines of text:")
+        for i in range(x):
+            line = input("> ")
+            file.write(line + "\n")
+        print("File created successfully!")
 
-  while True:
-    num_lines = int(input("Enter the number of lines (5-6): "))
-    if 5 <= num_lines <= 6:
-      for i in range(num_lines):
-        line = input("Enter line {}: ".format(i+1))
-        lines.append(line + "\n")  # Add newline for proper formatting
-      break
-    else:
-      print("Invalid number of lines. Please enter between 5 and 6.")
-  write_to_file(filename, lines)
+    upper_case_count, lower_case_count, digit_count = count_characters(file_name)
 
-  with open(filename, 'r') as file:
-    text = file.read()
-
-  uppercase_count, lowercase_count, digit_count = count_characters(text)
-
-  print("\nText content (sample):\n", text)
-  print("\nCharacter counts:")
-  print("Uppercase:", uppercase_count)
-  print("Lowercase:", lowercase_count)
-  print("Digits:", digit_count)
+    print(f"File Details - ")
+    print(f"File Name: {file_name}")
+    print(f"Upper Case Characters: {upper_case_count}")
+    print(f"Lower Case Characters: {lower_case_count}")
+    print(f"Digits: {digit_count}")
 
 if __name__ == "__main__":
-  main()
-
+    main()
