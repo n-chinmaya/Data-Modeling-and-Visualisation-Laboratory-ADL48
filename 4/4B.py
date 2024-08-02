0a1e64140bb002797f2397c400a2aa67a1d4c364
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2024-08-02 22:50:31 +0530

    shorter

--- 4/4B.py
+++ 4/4B.py
@@ -1,14 +1,10 @@
 import re
+text = input("Enter the text: ")
 
-def count_occurrences(text):
-  vowel_count = len(re.findall(r'[aeiouAEIOU]', text))
-  consonant_count = len(re.findall(r'[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]', text))
-  digit_count = len(re.findall(r'\d', text))
-  return {'vowels': vowel_count, 'consonants': consonant_count, 'digits': digit_count}
+vowels = re.findall(r'[aeiouAEIOU]', text)
+consonants = re.findall(r'[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]', text)
+digits = re.findall(r'[0-9]', text)
 
-text = "This is a sample text with vowels, consonants and digits. 123"
-counts = count_occurrences(text)
-print("Vowels:", counts['vowels'])
-print("Consonants:", counts['consonants'])
-print("Digits:", counts['digits'])
-
+print("Vowels:", len(vowels))
+print("Consonants:", len(consonants))
+print("Digits:", len(digits))
