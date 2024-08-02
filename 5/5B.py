a24b29b57302d574cf9c6ced77e4c5970abeba08
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2024-08-02 23:30:47 +0530

    shorter

--- 5/5B.py
+++ 5/5B.py
@@ -1,23 +1,13 @@
-#Thank You Nipun for making this code short!
-def create_text_file():
-    file = open("user_text.txt", "w")
-    x=int(input("Enter the number of lines (5-6): "))
-    for i in range(x):
-        text = input("Enter line " + str(i+1) + ": ")
-        file.write(text + "\n")
-    file.close()
+file = open("file.txt", "w")
+for i in range(5):
+  text = input("Enter text: ")
+  file.write(text + "\n")
+file.close()
 
-def find_longest_shortest_words():
-    file = open("user_text.txt", "r")
-    text = file.read()
-    words = text.split()
-    longest_word = max(words, key=len)
-    shortest_word = min(words, key=len)
-    file.close()
-    print("Longest word: " + longest_word)
-    print("Length: " + str(len(longest_word)))
-    print("Shortest word: " + shortest_word)
-    print("Length: " + str(len(shortest_word)))
-
-create_text_file()
-find_longest_shortest_words()
+with open("file.txt", "r") as file:
+  text = file.read()
+  words = text.split()
+  longest = max(words, key=len)
+  shortest = min(words, key=len)
+  print("Longest word: ", longest ," with length " , len(longest))
+  print("Shortest word: ", shortest ," with length " , len(shortest))
