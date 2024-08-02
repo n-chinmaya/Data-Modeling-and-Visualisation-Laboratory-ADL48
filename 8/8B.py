2c77e7b0bf5919565b6eb2d6deb6e2ea340894fd
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2024-08-03 01:05:52 +0530

    easier

--- 8/8B.py
+++ 8/8B.py
@@ -1,28 +1,24 @@
-def count_characters(file_name):
-    with open(file_name, 'r') as file:
-        text = file.read()
-        upper_case_count = sum(1 for char in text if char.isupper())
-        lower_case_count = sum(1 for char in text if char.islower())
-        digit_count = sum(1 for char in text if char.isdigit())
-        return upper_case_count, lower_case_count, digit_count
+file = open("file.txt", "w")
+for i in range(5):
+  text = input("Enter text: ")
+  file.write(text + "\n")
+file.close()
 
-def main():
-    file_name = "user_input.txt"
-    with open(file_name, 'w') as file:
-        x=int(input("Enter the number of lines (5-6): "))
-        print("Enter 5-6 lines of text:")
-        for i in range(x):
-            line = input("> ")
-            file.write(line + "\n")
-        print("File created successfully!")
+with open("file.txt", "r") as file:
+  text = file.read()
+  words = text.split()
+  Upper = 0
+  lower = 0 
+  digits = 0
+  for word in words:
+      for char in word:
+          if char.isupper():
+              Upper+=1
+          elif char.islower():
+              lower+=1
+          elif char.isdigit():
+              digits+=1
 
-    upper_case_count, lower_case_count, digit_count = count_characters(file_name)
-
-    print(f"File Details - ")
-    print(f"File Name: {file_name}")
-    print(f"Upper Case Characters: {upper_case_count}")
-    print(f"Lower Case Characters: {lower_case_count}")
-    print(f"Digits: {digit_count}")
-
-if __name__ == "__main__":
-    main()
+  print("Number of uppercase letters: ", Upper)
+  print("Number of lowercase letters: ", lower)
+  print("Number of digits: ", digits)
